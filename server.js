dfaf4a4c492036bd7b1d84eee3b28b6b62ca02e8
Ramath <ramathmanjitha@gmail.com>
2022-05-04 20:09:52 +0530

    customer commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const ItemRoutes = require('./trader/routes/item.routes');
 const WishList = require('./customer/routes/wishlist.routes')
 const caetListRoutes = require('./customer/routes/cart.routes')
 const customerRutes = require('./customer/routes/customer.routes')
+const traderRutes = require('./trader/routes/trader.routes')
 
 const app = new Koa();
 app.use(bodyParser());
@@ -16,6 +17,8 @@ app.use(ItemRoutes.routes())
     .use(caetListRoutes.allowedMethods())
     .use(customerRutes.routes())
     .use(customerRutes.allowedMethods())
+    .use(traderRutes.routes())
+    .use(traderRutes.allowedMethods())
 
 app.listen(3001);
 console.log('Application is running on port 3000');
